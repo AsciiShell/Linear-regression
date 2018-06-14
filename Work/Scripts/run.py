f61da114acd07da97aaefc32011b063f4736b04d
asciishell <asciishell@yandex.ru>
2018-06-14 11:44:34 +0300

    Run alert

--- Work/Scripts/run.py
+++ Work/Scripts/run.py
@@ -7,15 +7,19 @@
 """
 import os
 import sys
+import webbrowser
 
 if __name__ == "__main__":
     os.chdir("..")
     sys.path.append(os.path.abspath(os.path.curdir))
     from Library.weblib import load_config
+    address = load_config()
     sys.argv.append("runserver")
-    sys.argv.append(load_config())
+    sys.argv.append(address)
     sys.argv.append("--noreload")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    print("\nЗапуск сервера\n")
+    webbrowser.get().open("http://127.0.0.1:" + address.split(":")[1], new=2)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
